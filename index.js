5712bcef43e126a437d0e1c39181da3aa86698d3
Xander <annecy169@gmail.com>
2019-01-14 13:42:27 +0000

    Remove logs

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.get('/', function(req, res, next) {
     var date = new Date()
     setdate = moment(date, 'YYYY-MM-DD HH:mm:ss');
-    console.log("Set - " + date);
     res.send("Set Successful");
     res.end();
 });
@@ -18,10 +17,8 @@ app.get('/', function(req, res, next) {
 app.get('/check', function(req, res, next) {
     var checkdate = moment(new Date(), 'YYYY-MM-DD HH:mm:ss');
     var duration = moment.duration(checkdate.diff(setdate));
-    console.log("Check - " + duration.asMinutes());
     res.send(duration.asMinutes().toString());
     res.end();
 });
 
-console.log("App Started on port " + port);
 app.listen(port)
